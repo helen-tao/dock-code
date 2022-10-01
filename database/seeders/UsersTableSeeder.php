a40e00eda57c86e89587d178173ca210690bea08
helen <yun.helen.tao@gmail.com>
2022-10-02 08:32:07 +1000

    delete users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name='helen';
         $user->email='yun.helen.tao@gmail.com';
+        $user->is_admin=true;
         $user->save();
     }
 }
